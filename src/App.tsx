145783fb12a80b9c16a0e7615691ac93070e9d1e
Gon159x <gonzaxx5@gmail.com>
2025-04-23 15:54:52 -0300

    Feat:
    - Creado boton para ir al repositorio de github con un modal y una contraseña

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { GithubFloatingButton } from './components/GithubFloatingButton';
 import { Lienzo } from './components/Lienzo/Lienzo';
 import { MouseContextProvider } from './context/MouseContext';
 
@@ -9,9 +10,13 @@ import { MouseContextProvider } from './context/MouseContext';
 function App() {
   return (
     <>
+      {/* pongo el contexto del provider de esta forma porque creo 
+      que solo lo necesitare en el lienzo de otra forma necesitaria un hijo 
+      unico con <></>*/}
       <MouseContextProvider>
         <Lienzo />
       </MouseContextProvider>
+      <GithubFloatingButton />
     </>
   );
 }
